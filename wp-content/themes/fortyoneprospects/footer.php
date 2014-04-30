c47ea6c95280bf65906f684f70948a369f608c3d
Marcello Prattico <chimp@astrochimp.com>
2014-04-29 22:28:54 -0400

    adding footer and changing styles

--- wp-content/themes/fortyoneprospects/footer.php
+++ wp-content/themes/fortyoneprospects/footer.php
@@ -11,10 +11,24 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="site-info clearfix">
 			
-			
-			
+			<div class="getintouch">
+
+ 
+				 <button type="button" class="btn-lg" href="/contact"><span class="glyphicon glyphicon-arrow-right icon-small"></span> Get in Touch</button>
+				 
+				 <br />
+
+				 SEEN ENOUGH? EMAIL US!
+
+			</div>
+
+
+			<div class="boilerplate">
+
+				&copy; 2014 | 41 PROSPECTS, LLC | SYRACUSE NY
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
